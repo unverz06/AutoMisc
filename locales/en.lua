d24c8cae8062fc599e463458010921080a17a350
unverz06 <>
2022-11-01 16:52:18 +0100

    feat(): add locales

---
+++ locales/en.lua
@@ -0,0 +1,18 @@
+local _, L = ...;
+
+L.WELCOME = {
+    HI = "Hi ",
+    LOADED = ", AutoMiscellaneous is loaded.",
+  }
+  
+  L.TITLE = "AutoMiscellaneous — "
+  L.SELL_GREY = "Sale: "
+  L.REPAIR_MONEY = "Repair: not enough gold"
+  L.REPAIR_OK = "Repair: "
+  
+  L.OPTIONS = {
+    THANKS = "Thanks to use AutoMiscellaneous.",
+    TICKET = "If you have bug, please create ticket in my github or comment on curseforge",
+    SUPPORT = "All donations via Paypal are welcome to keep this project going!",
+    COMMAND = ""
+  }
---
+++ locales/fr.lua
@@ -0,0 +1,21 @@
+local _, L = ...;
+if GetLocale() == "frFR" then
+
+    L.WELCOME = {
+    HI = "Hi ",
+    LOADED = ", AutoMiscellaneous is loaded.",
+    }
+
+    L.TITLE = "AutoMiscellaneous — "
+    L.SELL_GREY = "Sale: "
+    L.REPAIR_MONEY = "Repair: not enough gold"
+    L.REPAIR_OK = "Repair: "
+
+    L.OPTIONS = {
+    THANKS = "Thanks to use AutoMiscellaneous.",
+    TICKET = "If you have bug, please create ticket in my github or comment on curseforge",
+    SUPPORT = "All donations via Paypal are welcome to keep this project going!",
+    COMMAND = ""
+    }
+
+end
